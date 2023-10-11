58096d505868e98fe7e0d43cec0979c7089ae4ab
Mr. Lamont <adam_lamont@wrdsb.ca>
2023-10-11 09:20:56 -0400

    Updated Example

--- Map.java
+++ Map.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+
 import java.awt.image.BufferedImage;
 
 public class Map {
@@ -9,7 +12,63 @@ public class Map {
     private Scene currentScene;
 
     public Map(String filename){
-       
+       // create the Scanner to read from the file
+       Scanner input = null;
+       // try to open the file in the scanner
+       try{
+            input = new Scanner(new File(filename));
+       }catch(Exception e){
+            // if there are errors, print them
+            e.printStackTrace();
+       }
+       // read the number of rows and columns
+       int numRows = input.nextInt();
+       // move to the nextLine
+       input.nextLine();
+       int numCols = input.nextInt();
+       input.nextLine();
+       // make our scene array
+       int numScenes = numRows * numCols;
+       this.scenes = new Scene[numScenes];
+       // read in the starting position
+       this.currentRow = input.nextInt();
+       input.nextLine();
+       this.currentCol = input.nextInt();
+       input.nextLine();
+
+       // read in all of the scene information
+       for(int i = 0; i < numScenes; i++){
+        // grab the image name
+        String imageName = input.next();
+        // load the image
+        BufferedImage image = null;
+        try{
+            image = ImageIO.read(new File("images//" + imageName));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        // remove the .png from the end
+        imageName = imageName.replace(".png","");
+        String[] sceneData = imageName.split("-");
+        // getting the row and column out of the picture name
+        int row = Integer.parseInt(sceneData[1]);
+        int col = Integer.parseInt(sceneData[3]);
+
+        // grab the directional data
+        boolean moveNorth = input.nextBoolean();
+        boolean moveEast = input.nextBoolean();
+        boolean moveSouth = input.nextBoolean();
+        boolean moveWest = input.nextBoolean();
+
+        // get the description and move to the next row
+        String description = input.nextLine();
+
+        // make the scene and store it in the array
+        Scene s = new Scene(row, col, image, moveNorth, 
+                    moveEast, moveSouth, moveWest, 
+                    description );
+        this.scenes[i] = s;
+       }
 
     }
 
--- Scene.java
+++ Scene.java
@@ -12,40 +12,51 @@ public class Scene {
     private boolean moveWest;
     private String description;
 
-    public Scene(int row, int col, 
+    public Scene(int row, int col, BufferedImage image, 
+       boolean moveNorth, boolean moveEast, 
+       boolean moveSouth, boolean moveWest, 
+       String description){
+        this.row = row;
+        this.col = col;
+        this.image = image;
+        this.moveNorth = moveNorth;
+        this.moveEast = moveEast;
+        this.moveSouth = moveSouth;
+        this.moveWest = moveWest;
+        this.description = description;
        
     }
 
 
     public int getRow(){
-        
+        return this.row;
     }
 
     public int getCol(){
-        
+        return this.col;
     }
 
     public BufferedImage getImage(){
-        
+        return this.image;
     }
 
     public boolean canMoveNorth(){
-       
+       return this.moveNorth;
     }
 
     public boolean canMoveEast(){
-       
+       return this.moveEast;
     }
 
     public boolean canMoveSouth(){
-        
+        return this.moveSouth;
     }
 
     public boolean canMoveWest(){
-        
+        return this.moveWest;
     }
 
     public String getDescription(){
-        
+        return this.description;
     }
 }
